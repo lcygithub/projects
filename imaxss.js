ffaaec41d72e90cd6f806f7cfde97c5c8f7018b2
lcygithub <lcyang_2011@163.com>
2013-11-03 22:21:24 +0800

    Update imaxss.js

--- imaxss.js
+++ imaxss.js
@@ -18,8 +18,9 @@ xss.prototype = {
 	del : function (callback) {
 		var ids = $(".reply").map(function(data, index) {return (index.id+"").split("-")[1]});
 		var tid = location.href.split("&").map(function(index, data) {if(index.match("id")){return index;}})[1].split("=")[1];
-		for(var i = 0, l = ids.length; i<l; i++) {
+		for (var i = 0, l = ids.length; i<l; i++) {
 			var url = "/?c=ajax&a=delReply&id=" + ids[i]+"" + "&tid=" + tid+"";
+			console.log("delReply:" + url);
 			setTimeout(get(url), 20);
 		}
 		function get(url) {
