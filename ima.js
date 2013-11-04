64330e24bbdbbdcec86351777cfcf1048a77054e
lcygithub <lcyang_2011@163.com>
2013-11-04 10:39:06 +0800

    Update ima.js

--- ima.js
+++ ima.js
@@ -1,2 +1,7 @@
+$('.left').parent().append($('.right'));
+$('.right').show();
+$('.reply-num').text('1 个回复');
+$('.ke-container.ke-container-default').css('width','');
+$('#submit-div').css('width','660px');
 $('.tiezi-main>.right').css("width","225px");
 console.log(document.cookie);
